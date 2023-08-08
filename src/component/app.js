c2b85cb7e5ff493604535dfcb73891ac824f58a7
Protap Ghosh <protapg518@gmail.com>
2023-08-09 00:16:13 +0600

    design security and oversight section

---
+++ src/component/app.js
@@ -0,0 +1,10 @@
+
+const Container = ({children}) => {
+   return (
+      <div className="max-w-[2520px] mx-auto xl:px-16 md:px-10 sm:px-2 px-4">
+         {children}
+      </div>
+   );
+};
+
+export default Container;
