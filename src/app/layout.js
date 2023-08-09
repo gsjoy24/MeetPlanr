4911341a74ca3691845148db4cbd9fb6febfc028
Sayed Hossain Bhuiyan <mdshiam4@gmail.com>
2023-08-10 01:50:13 +0600

    done

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,7 +12,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={poppins.className}>
-        <h2>siam</h2>
         <Navbar></Navbar>
         {children}
         <Footer></Footer>
