2dd8d7f8b166493873158396acce3cfab759f250
jmjubaer <jmjubaer3927@gmail.com>
2023-08-09 10:05:29 +0600

    add sectionTitle and featured section

---
+++ src/Featured/Featured.js
@@ -0,0 +1,15 @@
+import SectionTitle from '@/components/SectionTitle';
+import React from 'react';
+
+const Featured = () => {
+    return (
+        <section className='' style={{marginTop: "64px",marginBottom: "64px"}}>
+            <SectionTitle title={"Features"}></SectionTitle>
+            <div className="">
+                <h1>dfadf</h1>
+            </div>
+        </section>
+    );
+};
+
+export default Featured;
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import Featured from "@/Featured/Featured";
+
 export default function Home() {
 	return (
-		<div className="h-[100vh] w-full flex justify-center items-center">
-			<div>
-				<h1 className="text-4xl">Developing MeetPlanr</h1>
-				<p className="text-right text-sm mt-2">web coders</p>
+		<div>
+			<div className="max-w-[2520px] mx-auto xl:px-16 md:px-10 sm:px-2 px-4">
+				<h1>dfh</h1>
+				<Featured></Featured>
 			</div>
 		</div>
 	);
