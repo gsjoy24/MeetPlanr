204ff6a80f06282504adcae8d89b99743fcab72f
jmjubaer <jmjubaer3927@gmail.com>
2023-08-09 10:08:43 +0600

    resolved marge confilct

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,12 @@
 import Featured from "@/Featured/Featured";
-
-export default function Home() {
-	return (
-		<div>
-			<div className="max-w-[2520px] mx-auto xl:px-16 md:px-10 sm:px-2 px-4">
-				<h1>dfh</h1>
-				<Featured></Featured>
 import Security from "@/security/app";
 
 const Home = () => {
 	return (
 		<div className="">
 			<div>
-        <Security></Security>
+				<Featured></Featured>
+        		<Security></Security>
 			</div>
 		</div>
 	);
