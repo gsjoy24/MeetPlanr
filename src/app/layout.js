7ded54fdea9eaf5bcf210158e41e6e0b9738baa1
Sayed Hossain Bhuiyan <mdshiam4@gmail.com>
2023-08-09 23:16:18 +0600

    new barnch

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,7 +9,10 @@ import { poppins } from '@/components/fonts'
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={poppins.className}>{children}</body>
+      <h2>siam</h2>
+      <body className={poppins.className}>
+        {children}
+      </body>
     </html>
   ) 
 }
