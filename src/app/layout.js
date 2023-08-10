a02024a2cbf9049a756831f57c994635984ee2b6
jmjubaer <jmjubaer3927@gmail.com>
2023-08-10 14:08:03 +0600

    pull and update code

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,6 +14,7 @@ export default function RootLayout({ children }) {
       <body className={poppins.className}>
         <Navbar></Navbar>
         {children}
+        <Footer></Footer>
       </body>
     </html>
   ) 
