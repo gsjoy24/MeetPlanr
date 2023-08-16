502910ba58d95a4c0b2f3703bfe4bc90e8010e1e
jmjubaer <jmjubaer3927@gmail.com>
2023-08-16 10:25:17 +0600

    add button component

--- src/components/SectionTitle.js
+++ src/components/SectionTitle.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import { bebas_neue } from '@/components/fonts'
+import React from "react";
+import { bebas_neue } from "@/utils/fonts";
 
-const SectionTitle = ({title}) => {
+const SectionTitle = ({ title }) => {
     return (
         <div>
-            <h2 className={`${bebas_neue.className} uppercase text-5xl text-center mb-16`}>{title}</h2>
+            <h2
+                className={`${bebas_neue.className} uppercase text-5xl text-center mb-16`}
+            >
+                {title}
+            </h2>
         </div>
     );
 };
