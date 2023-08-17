6b6dc1b7c0546dcf140a83721bdba23dd06d52dc
gourjoy <goursaha307@gmail.com>
2023-08-18 04:25:08 +0600

    previewed

--- src/shereComoponets/Navbar.jsx
+++ src/shereComoponets/Navbar.jsx
@@ -1,16 +1,21 @@
 "use client"
 import Button from "@/common/Button";
 import Container from "@/components/container";
+import { UserAuth } from "@/providers/AuthProvider";
 import Link from "next/link";
 import React, { useState } from "react";
 import { FaBars, FaTimes } from 'react-icons/fa';
 
 const Navbar = () => {
     const [open, setOpen] = useState(false);
+
+    // todo: this is the user info, design the interface now. Ans Please remove the unnecessary classes {jm-shadow, jm_nav}. This file should have the extension js, not jsx i think. best of luck!
+    const { user } = UserAuth()
+    console.log(user);
     return (
         <Container>
             <div>
-                <nav className='flex lg:grid lg:grid-cols-5 justify-between items-center py-6 px-4 border-b-2  border-s-black '>
+                <nav className='flex lg:grid lg:grid-cols-5 justify-between items-center py-6 px-4 border-b-2 border-s-black '>
                     <h1 className='text-4xl font-bold text-[#465AF7] text-blue-e lg:col-span-2 jm-shadow mx-2'><Link href="/">MeetPlanr</Link></h1>
 
                     <div className={`jm_nav ${open ? "w-4/5 md:w-1/2 p-5" : "w-0"}`}>
