68c4233c8eb95697fb6dc7903302a050df88c782
Sayed Hossain Bhuiyan <mdshiam4@gmail.com>
2023-08-17 17:08:53 +0600

    add new

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,9 @@
 import Banner from "@/Banner/Banner";
 import Featured from "@/Featured/Featured";
 import PricingPlan from "@/PricingPlan/PricingPlan";
-import Security from "@/security/app";
+// import Security from "@/security/app";
 import Counting from "@/Counting/Information";
+import Review from "@/Review/page";
 
 const Home = () => {
 	return (
@@ -12,8 +13,9 @@ const Home = () => {
 				<Banner></Banner>
 				<Featured></Featured>
 				<Counting></Counting>
-				<Security></Security>
+				{/* <Security></Security> */}
 				<PricingPlan></PricingPlan>
+				<Review></Review>
 			</div>
 
 		</div>
