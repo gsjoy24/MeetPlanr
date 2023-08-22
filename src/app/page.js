927a19480b6bbdf292499782c2724668ea60afb6
Protap Ghosh <protapg518@gmail.com>
2023-08-22 21:44:20 +0600

    change product section button and banner color

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,4 @@
-
 import Featured from "@/Featured/Featured";
-import PricingPlan from "@/PricingPlan/PricingPlan";
 import Counting from "@/Counting/Information";
 import Banner from "@/Banner/Banner";
 import Review from "@/Review/Review";
