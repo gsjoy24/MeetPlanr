39ff77131cefc1d7d36b0bc864ac5d4a02932e9a
jmjubaer <jmjubaer3927@gmail.com>
2023-08-22 07:43:02 +0600

    Change form design

--- src/app/event/[formType]/page.js
+++ src/app/event/[formType]/page.js
@@ -42,18 +42,18 @@ const page = ({params}) => {
                     <button onClick={()=> router.back()} className='border border-[#465AF7] text-[#465AF7] w-fit h-fit mx-auto px-5 py-4 rounded-3xl hover:bg-[#465AF7] hover:text-[#fff] cursor-pointer transition-all duration-500'><FaAngleLeft className='inline mr-1'/> Back</button>
                 </div>
     {/*================= Event Form ====================== =*/}
-                <div className='w-4/5 border-2 rounded-md shadow-md mx-auto p-5'>
+                <div className='w-4/5 border-2 rounded-md shadow-md mx-auto p-5 bg-[#edf1f4]'>
                     {/* <h2 className='text-3xl font-medium text-center w-full'>Add {params.formType} Event Type</h2> */}
                     <h2 className='text-3xl font-medium text-center w-full'>Add One-on-One Event Type</h2>
 
-                    <form className='flex flex-col w-4/5 mx-auto mt-8' onSubmit={handleSubmit(onSubmit)}>
+                    <form className='flex flex-col w-8/12 mx-auto mt-8' onSubmit={handleSubmit(onSubmit)}>
 
                         <label className='text-lg font-semibold mb-3' htmlFor='eventName'>Event Name:*</label>
-                        <input placeholder='Event Name...' required className='input_field' id='eventName' {...register("eventName")} />         
+                        <input placeholder='Event Name...' required className='input_field disc_effects' id='eventName' {...register("eventName")} />         
 
                         <label className='text-lg font-semibold mb-3 mt-5' htmlFor='eventMethod'>Event Method:*</label> 
                         
-                        <div className="input_field mt-8 relative">
+                        <div className="input_field disc_effects mt-8 relative">
                             {
                                 (method == "Google Meet") ? 
                                 
@@ -95,10 +95,10 @@ const page = ({params}) => {
                         </div>
 
                         <label className='text-lg font-semibold mb-3 mt-8' htmlFor='description'>Description:</label>
-                        <textarea placeholder='Write your schedule details....' id='description' className='input_field min-h-[200px] resize-none' {...register("description")}></textarea>
+                        <textarea placeholder='Write your schedule details....' id='description' className='input_field disc_effects min-h-[200px] resize-none' {...register("description")}></textarea>
 
                         <label className='text-lg font-semibold mb-3 mt-8' htmlFor='duration'>Duration:*</label>
-                        <select id='duration' className='input_field' {...register("duration")}>
+                        <select id='duration' className='input_field disc_effects' {...register("duration")}>
                             <option value="15">15</option>
                             <option value="30">30</option>
                             <option value="45">45</option>
@@ -108,11 +108,11 @@ const page = ({params}) => {
                         <div className="grid grid-cols-2 gap-5 mt-8">
                             <div className="">
                                 <label className='text-lg font-semibold mb-3' htmlFor='eventDate'>Event Date:*</label>
-                                <input className='input_field' id='eventDate' {...register("eventDate")} type='date' required/> 
+                                <input className='input_field disc_effects' id='eventDate' {...register("eventDate")} type='date' required/> 
                             </div>
                             <div className="">
                                 <label className='text-lg font-semibold mb-3' htmlFor='eventTime'>Event Time:*</label>
-                                <input className='input_field' id='eventTime' {...register("eventTime")} type='time' required/> 
+                                <input className='input_field disc_effects' id='eventTime' {...register("eventTime")} type='time' required/> 
                             </div>
                         </div>
                         <input className='mp_btn mt-8' type="submit" />
