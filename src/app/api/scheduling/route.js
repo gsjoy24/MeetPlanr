fff04ec0d1b2252feca1ce5af1d9ac80dd664204
gourjoy <goursaha307@gmail.com>
2023-08-23 14:21:40 +0600

    fixed

--- src/app/api/scheduling/route.js
+++ src/app/api/scheduling/route.js
@@ -1,21 +1,20 @@
-import DbConnect from "@/services/DbConnect";
-import { NextResponse } from "next/server";
+import DbConnect from '@/services/DbConnect';
+import { NextResponse } from 'next/server';
 
-export const POST= async(request)=>  {
-    if (request.method === "POST") {
-  
-      try {
-        const body =await request.json();
-        const db = await DbConnect();
-        const schedulingCollection = db.collection("scheduling");
-        const {eventName,method,description,duration,eventDate,eventTime,scheduleLink}=body;
-        const docs={eventName,method,description,duration,eventDate,eventTime,scheduleLink};
-      const result=await schedulingCollection.insertOne(docs);
-        return NextResponse.json(result);
-      } catch (error) {
-        NextResponse.json({ error: "Failed to insert data" });
-      }
-    } else {
-      NextResponse.json({ message: "Method not allowed" });
-    }
-  }
+export const POST = async (request) => {
+	if (request.method === 'POST') {
+		try {
+			const body = await request.json();
+			const db = await DbConnect();
+			const schedulingCollection = db.collection('scheduling');
+			const { eventName, method, description, duration, eventDate, eventTime, scheduleLink } = body;
+			const docs = { eventName, method, description, duration, eventDate, eventTime, scheduleLink };
+			const result = await schedulingCollection.insertOne(docs);
+			return NextResponse.json(result);
+		} catch (error) {
+			NextResponse.json({ error: 'Failed to insert data' });
+		}
+	} else {
+		NextResponse.json({ message: 'Method not allowed' });
+	}
+};
