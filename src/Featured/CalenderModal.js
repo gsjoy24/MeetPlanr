08b844c04ebb795e4fc03bc541dadd43df99d9d6
Protap Ghosh <protapg518@gmail.com>
2023-08-25 03:05:05 +0600

    Modal content added

--- src/Featured/CalenderModal.js
+++ src/Featured/CalenderModal.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import { IoIosCloseCircleOutline } from "react-icons/io";
 
 const CalenderModal = ({ isVisible, onClose }) => {
@@ -15,11 +15,31 @@ const CalenderModal = ({ isVisible, onClose }) => {
                   <IoIosCloseCircleOutline className="text-[30px] text-red-400 hover:text-red-500 duration-400 font-thin" />
                </button>
 
-               <div>
-                  <p>Modal content</p>
-                  <p>Modal content</p>
-                  <p>Modal content</p>
-                  <p>Modal content</p>
+               {/* These texts are for arrangement purposes only, they should be
+               deleted when adding the calendar. */}
+               <div className="mt-8 p-5">
+                  <h2 className="text-3xl font-bold mb-3">
+                     Scheduling Calendar
+                  </h2>
+                  <p>
+                     There are many variations of passages of Lorem Ipsum
+                     available, but the majority have suffered alteration in
+                     some form, by injected humour, or randomised words which do
+                     not look even slightly believable. If you are going to use
+                     a passage of Lorem Ipsum, you need to be sure there is not
+                     anything embarrassing hidden in the middle of text.
+                  </p>
+                  <br />
+                  <p>
+                     All the Lorem Ipsum generators on the Internet tend to
+                     repeat predefined chunks as necessary, making this the
+                     first true generator on the Internet. It uses a dictionary
+                     of over 200 Latin words, combined with a handful of model
+                     sentence structures, to generate Lorem Ipsum which looks
+                     reasonable. The generated Lorem Ipsum is therefore always
+                     free from repetition, injected humour, or
+                     non-characteristic words etc.
+                  </p>
                </div>
             </div>
          </div>
