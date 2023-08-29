00c9ed7b86233598641e719f534f5e2f972e98c8
jmjubaer <jmjubaer3927@gmail.com>
2023-08-29 07:45:56 +0600

    change database

--- src/app/event/[formType]/page.js
+++ src/app/event/[formType]/page.js
@@ -12,22 +12,33 @@ import meetlogo from '../../../assets/event-form/meet.png'
 import locationLogo from '../../../assets/event-form/location.png'
 import Image from 'next/image';
 import { UserAuth } from '@/providers/AuthProvider';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const page = ({params}) => {
     const {register,handleSubmit,reset} = useForm()
     const router = useRouter();
     const [method,setMethod] = useState(null);
+    const [dateRange,setDateRange] = useState(null);
     const [action,setAction] = useState(false);
     const { user } = UserAuth()
-    // console.log(user);
-    const email=user?.email;
-    const name=user?.displayName;
-    // console.log(method);
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(null);
+    const onChange = (dates) => {
+    const [start, end] = dates;
+      setStartDate(start);
+      setEndDate(end);
+    }
+    const clearDateRange = () => {
+        setDateRange(null);
+        setStartDate(new Date())
+        setEndDate(null)
+    }
     const onSubmit = async (data) => {
-        console.log(data)
-    //     // console.log(scheduleLink);
       if(method){
         const {eventName,description,duration,eventDate,eventTime,eventLink}=data;
+        const email=user?.email;
+        const name=user?.displayName;
         const x= Math.round(Math.random() * 100000)
         const scheduleLink = `http://localhost:3000/${eventLink + x}`;
         try {
@@ -90,7 +101,7 @@ const page = ({params}) => {
                                 <div className="flex gap-3 relative">
                                     <Image className='w-6 object-contain rounded-full' alt='meet icon' src={locationLogo}/>
                                     <p className='text-lg font-medium'>In-person Meeting</p>
-                                    <span onClick={() => setMethod(null)} className='absolute right-1 top-1 text-lg cursor-pointer'><FaTimes/></span>
+                                    <span onClick={() => setMethod(null)} className='absolute right-0 top-1 text-lg cursor-pointer'><FaTimes/></span>
                                 </div>
 
                                 : <div onClick={() => setAction(!action)} className="cursor-pointer">
@@ -115,7 +126,7 @@ const page = ({params}) => {
                                 </div>
                             }
                         </div>
-
+                    
                         <label className='text-lg font-semibold mb-3 mt-8' htmlFor='description'>Description:</label>
                         <textarea placeholder='Write your schedule details....' id='description' className='input_field min-h-[200px] resize-none' {...register("description")}></textarea>
 
@@ -127,20 +138,56 @@ const page = ({params}) => {
                             <option value="60">60</option>
                         </select>
 
-                        <div className="grid sm:grid-cols-2 gap-5 mt-8">
-                            <div className="">
-                                <label className='text-lg font-semibold mb-3' htmlFor='eventDate'>Event Date:*</label>
-                                <input className='input_field' id='eventDate' {...register("eventDate")} type='date' required/> 
-                            </div>
-                            <div className="">
-                                <label className='text-lg font-semibold mb-3' htmlFor='eventTime'>Event Time:*</label>
-                                <input className='input_field' id='eventTime' {...register("eventTime")} type='time' required/> 
-                            </div>
+                        <label className='text-lg font-semibold mb-3 mt-8'>Date Range:*</label>
+                        <div className={`rounded-md p-3 w-full relative 
+                        ${action ? "border-2 border-[#00a4f8]" : "border border-gray-800"}`}>
+                            {
+                                (dateRange == "Google Meet") ? 
+                                
+                                <div className="flex gap-3 relative">
+                                    <p className='text-lg font-medium'>Indefinitely into the future</p>
+                                    <span onClick={() => setDateRange(null)} className='absolute right-1 top-1 text-lg cursor-pointer'><FaTimes/></span>
+                                </div>
+                                
+                                :(dateRange == "In Person") ?                                    
+                                
+                                <div className="flex gap-3 relative">
+                                        <DatePicker
+                                            selected={startDate}
+                                            onChange={onChange}
+                                            startDate={startDate}
+                                            endDate={endDate}
+                                            selectsRange
+                                            withPortal
+                                            />
+                                    <span onClick={clearDateRange} className='absolute right-1 top-1 text-lg cursor-pointer'><FaTimes/></span>
+                                </div>
+
+                                : <div onClick={() => setAction(!action)} className="cursor-pointer">
+                                    <div className="flex justify-between items-center">
+                                        <p> Date Range</p>
+                                        {
+                                            action ? <FaAngleUp className='text-xl'/> : <FaAngleDown className='text-xl'/>
+                                        }
+                                    </div>
+                                    {
+                                        action && <div className="">
+                                            <div onClick={() => setDateRange('Google Meet')} className="flex gap-3 border-y-2 border-[#00a4f8] my-2 py-2">
+                                                <p className='text-lg font-medium'>Indefinitely into the future</p>
+                                            </div>
+                                            <div onClick={() => setDateRange('In Person')} className="flex gap-3">
+                                                <p className='text-lg font-medium'>Select a date range</p>
+                                            </div>
+                                        </div>
+                                    }
+                                </div>
+                            }
                         </div>
 
                         <label className='text-lg font-semibold mb-3 mt-8' htmlFor='eventLink'>Event Link:*</label>
+                        <label className='font-medium my-2' htmlFor='eventLink'>http://localhost:3000/user</label>
                         <div className="input_field flex gap-1 overflow-hidden">
-                            <label className='font-medium' htmlFor='eventLink'>http://localhost:3000/</label>
+                        <label className='font-medium my-2' htmlFor='eventLink'>/jmjubaer/</label>
                             <input placeholder='Link path...' required className='outline-none' id='eventLink' {...register("eventLink")} />
                         </div>  
                     </div>
