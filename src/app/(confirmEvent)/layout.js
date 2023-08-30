03911acb8d28240667ad5fe7abe5630262d53ca2
jmjubaer <jmjubaer3927@gmail.com>
2023-08-30 23:07:39 +0600

    solved hydretion error

--- src/app/(confirmEvent)/layout.js
+++ src/app/(confirmEvent)/layout.js
@@ -1,24 +1,10 @@
-import '../globals.css';
-export const metadata = {
-	title: 'MeetPlanr',
-	description:
-		'MeetPlanr: The open-source solution for effortless meeting scheduling and planning. Sync calendars, leverage smart algorithms, and boost collaboration. Join us now! 🚀 #MeetPlanr #SchedulingMadeEasy'
-};
-import { poppins } from '@/utils/fonts';
-import { AuthProvider } from '@/providers/AuthProvider';
-import { Toaster } from 'react-hot-toast';
 
-export default function RootLayout({ children }) {
+export default function EventLayout({ children }) {
 	return (
-		<html lang="en">
-			<body className={poppins.className}>
-				<AuthProvider>
-					<div>
-						<main className="">{children}</main>
-					</div>
-					<Toaster position="top-center" reverseOrder={false} />
-				</AuthProvider>
-			</body>
-		</html>
+		<div>
+			<div>
+				<main className="min-h-[calc(100vh-200px)]">{children}</main>
+			</div>
+		</div>
 	);
 }
