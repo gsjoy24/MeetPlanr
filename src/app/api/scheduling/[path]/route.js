6ea673b5d47669ef28af35f1d0282c46b9ca1871
jmjubaer <jmjubaer3927@gmail.com>
2023-08-30 17:15:52 +0600

    add email send functionality

--- src/app/api/scheduling/[path]/route.js
+++ src/app/api/scheduling/[path]/route.js
@@ -5,10 +5,35 @@ export const GET = async (request,{params}) => {
     try {
         const db = await DbConnect();
         const {path} = params;
-        console.log(path);
         const query = {path : path}
         const schedulingCollection = db.collection("scheduling");
         const result = await schedulingCollection.findOne(query);
+        return NextResponse.json(result);
+    }
+    catch (error) {
+        console.error("error for geting data", error);
+        NextResponse.json({ error: "eroor for geting data" });
+    }
+};
+
+export const PUT = async (request,{params}) => {
+    try {
+        const db = await DbConnect();
+        const {path} = params;
+        const body =await request.json();
+        const {inviteeName,inviteeEmail,scheduleDate} = body || {};
+        const filter = {path : path}
+        const updateDoc = {
+            $set: {
+                inviteeName,
+                inviteeEmail,
+                scheduleDate,
+                confirm: true
+            },
+        }
+        const option = {upsert : true}
+        const schedulingCollection = db.collection("scheduling");
+        const result = await schedulingCollection.updateOne(filter,updateDoc,option);
         return NextResponse.json(result);
     }
     catch (error) {
