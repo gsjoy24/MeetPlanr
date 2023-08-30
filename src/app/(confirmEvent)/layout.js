026289ddc5f3206f8667b4e26f286d2dbdd45a44
jmjubaer <jmjubaer3927@gmail.com>
2023-08-30 22:06:57 +0600

    add both side email send functionality

---
+++ src/app/(confirmEvent)/layout.js
@@ -0,0 +1,24 @@
+import '../globals.css';
+export const metadata = {
+	title: 'MeetPlanr',
+	description:
+		'MeetPlanr: The open-source solution for effortless meeting scheduling and planning. Sync calendars, leverage smart algorithms, and boost collaboration. Join us now! 🚀 #MeetPlanr #SchedulingMadeEasy'
+};
+import { poppins } from '@/utils/fonts';
+import { AuthProvider } from '@/providers/AuthProvider';
+import { Toaster } from 'react-hot-toast';
+
+export default function RootLayout({ children }) {
+	return (
+		<html lang="en">
+			<body className={poppins.className}>
+				<AuthProvider>
+					<div>
+						<main className="">{children}</main>
+					</div>
+					<Toaster position="top-center" reverseOrder={false} />
+				</AuthProvider>
+			</body>
+		</html>
+	);
+}
