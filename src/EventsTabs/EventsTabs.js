12db60c227d1282b23626c8ad6dd69677e9222d3
jmjubaer <jmjubaer3927@gmail.com>
2023-09-01 15:21:34 +0600

    add evn file

--- src/EventsTabs/EventsTabs.js
+++ src/EventsTabs/EventsTabs.js
@@ -54,7 +54,7 @@ const EventsTabs = () => {
 							<div>
 								<h4>Jhone Devlin</h4>
 								<Link href="" className="text-[#465AF7]">
-									https://meetplanr.com/alexaghosh6
+									https://meet-planr.vercel.app/{}
 								</Link>
 							</div>
 						</div>
