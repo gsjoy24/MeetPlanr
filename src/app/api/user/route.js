79ca63e5306607d175fd58812f4cb667524e0fba
jmjubaer <jmjubaer3927@gmail.com>
2023-09-02 15:11:37 +0600

    change user api name

--- src/app/api/user/route.js
+++ src/app/api/user/route.js
@@ -31,3 +31,16 @@ export const POST = async (request) => {
 		return NextResponse.json({ message: 'Method not allowed' }, { status: 405 });
 	}
 };
+
+export const GET = async (request) => {
+    try {
+        const db = await DbConnect();
+        const schedulingCollection = db.collection("users");
+        const result = await schedulingCollection.find().toArray();
+        return NextResponse.json(result);
+    }
+    catch (error) {
+        console.error("error for geting data", error);
+        NextResponse.json({ error: "eroor for geting data" });
+    }
+};
