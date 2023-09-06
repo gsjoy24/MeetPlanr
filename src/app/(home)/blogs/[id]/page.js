59435a32f04e1877a6fd4b718521bd96a623a9a2
gourjoy <goursaha307@gmail.com>
2023-09-06 19:04:55 +0600

    fixed

--- src/app/(home)/blogs/[id]/page.js
+++ src/app/(home)/blogs/[id]/page.js
@@ -1,5 +1,4 @@
-"use client"
-
+'use client';
 
 /* eslint-disable react-hooks/rules-of-hooks */
 import axios from 'axios';
@@ -7,37 +6,31 @@ import Image from 'next/image';
 import React, { useEffect, useState } from 'react';
 
 const page = ({ params }) => {
-    const [blog, setBlog] = useState({});
+	const [blog, setBlog] = useState({});
 
-    useEffect(() => {
-        async function fetchData() {
-            try {
-                const response = await axios(`/api/blog/${params.id}`);
-                setBlog(response.data);
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-        }
+	useEffect(() => {
+		async function fetchData() {
+			try {
+				const response = await axios(`/api/blog/${params.id}`);
+				setBlog(response.data);
+			} catch (error) {
+				console.error('Error fetching data:', error);
+			}
+		}
 
-        fetchData();
-    }, [params]);
+		fetchData();
+	}, [params]);
 
-    return (
-        <div className=" min-h-screen py-6">
-            <div className="max-w-2xl mx-auto  p-6 ">
-                <h2 className="text-2xl font-semibold mb-4">{blog.title}</h2>
-                <Image
-                    src={blog.image}
-                    alt={blog.title}
-                    className="w-full rounded-md mb-4"
-                    width={1000}
-                    height={400} 
-                />
-                <p className="text-gray-600">{blog.subtitle}</p>
-                <p className="text-gray-800 mt-2">{blog.content}</p>
-            </div>
-        </div>
-    );
+	return (
+		<div className=" min-h-screen py-6">
+			<div className=" max-w-2xl p-6 mx-auto">
+				<h2 className="mb-4 text-2xl font-semibold">{blog.title}</h2>
+				<Image src={blog.image} alt={blog.title} className="w-full mb-4 rounded-md" width={1000} height={400} />
+				<p className="text-gray-600">{blog.subtitle}</p>
+				<p className="mt-2 text-gray-800">{blog.content}</p>
+			</div>
+		</div>
+	);
 };
 
 export default page;
