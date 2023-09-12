22d89d770465554ddcbcef7a1002eb2125da9923
gourjoy <goursaha307@gmail.com>
2023-09-12 18:03:32 +0600

    admin page

--- src/app/(admin)/admin/page.js
+++ src/app/(admin)/admin/page.js
@@ -1,18 +1,12 @@
+import State from '@/components/State';
+
 const AdminPage = () => {
 	return (
 		<div className="flex flex-wrap items-center justify-center gap-5 text-white">
-			<div className="bg-[#465AF7] p-8 rounded-2xl min-w-[180px] flex flex-col justify-center items-center gap-3">
-				<span className="text-6xl font-bold">132</span> <span>Users</span>
-			</div>
-			<div className="bg-[#465AF7] p-8 rounded-2xl min-w-[180px] flex flex-col justify-center items-center gap-3">
-				<span className="text-6xl font-bold">42</span> <span>Blogs</span>
-			</div>
-			<div className="bg-[#465AF7] p-8 rounded-2xl min-w-[180px] flex flex-col justify-center items-center gap-3">
-				<span className="text-6xl font-bold">232</span> <span>Schedules</span>
-			</div>
-			<div className="bg-[#465AF7] p-8 rounded-2xl min-w-[180px] flex flex-col justify-center items-center gap-3">
-				<span className="text-6xl font-bold">121</span> <span>Feedbacks</span>
-			</div>
+			<State num={'132'} title="Users" />
+			<State num={'42'} title="Blogs" />
+			<State num={'232'} title="Schedules" />
+			<State num={'121'} title="Feedbacks" />
 		</div>
 	);
 };
