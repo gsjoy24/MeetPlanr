26ef3674ff4c16502e8fac493337ce6659c78742
gourjoy <goursaha307@gmail.com>
2023-09-12 19:06:56 +0600

    admin states complete

--- src/app/(admin)/admin/page.js
+++ src/app/(admin)/admin/page.js
@@ -2,11 +2,14 @@ import State from '@/components/State';
 
 const AdminPage = () => {
 	return (
-		<div className="flex flex-wrap items-center justify-center gap-5 text-white">
-			<State num={'132'} title="Users" />
-			<State num={'42'} title="Blogs" />
-			<State num={'232'} title="Schedules" />
-			<State num={'121'} title="Feedbacks" />
+		<div>
+			<h1 className=" md:text-2xl mb-5 text-xl font-bold">Admin Dashboard</h1>
+			<div className="flex flex-wrap items-center justify-center gap-5 text-white">
+				<State num={'132'} title="Users" />
+				<State num={'42'} title="Blogs" />
+				<State num={'232'} title="Schedules" />
+				<State num={'121'} title="Feedbacks" />
+			</div>
 		</div>
 	);
 };
--- src/components/State.js
+++ src/components/State.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import { FaUsers } from 'react-icons/fa';
+import { RiArticleFill } from 'react-icons/ri';
+import { BiSolidCommentDetail } from 'react-icons/bi';
+import { BsFillCalendar2WeekFill } from 'react-icons/bs';
 
 const State = ({ num, title }) => {
+	let stateIcon;
+	switch (title) {
+		case 'Users':
+			stateIcon = <FaUsers size={18} />;
+			break;
+		case 'Blogs':
+			stateIcon = <RiArticleFill size={18} />;
+			break;
+		case 'Schedules':
+			stateIcon = <BsFillCalendar2WeekFill size={16} />;
+			break;
+		case 'Feedbacks':
+			stateIcon = <BiSolidCommentDetail size={18} />;
+			break;
+		default:
+			stateIcon = null; // Render nothing for unsupported cases
+	}
 	return (
-		<div className="bg-[#465AF7] p-8 rounded-2xl min-w-[180px] flex flex-col justify-center items-center gap-3">
-			<span className="text-6xl font-bold">{num}</span> <span>{title}</span>
+		<div className="bg-[#465AF7] hover:bg-[#364cf7] duration-200 p-5 sm:p-8 rounded-2xl min-w-[145px] sm:min-w-[180px] flex flex-col justify-center items-center gap-2">
+			<span className="md:text-6xl text-5xl font-bold">{num}</span>
+			<span className="flex items-center justify-center gap-2">
+				{stateIcon}
+				{title}
+			</span>
 		</div>
 	);
 };
