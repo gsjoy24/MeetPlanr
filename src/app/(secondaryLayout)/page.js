c6d15d9ff07f85bd593166c98a96807184354c42
jmjubaer <jmjubaer3927@gmail.com>
2023-09-22 15:30:16 +0600

    d

--- src/app/(secondaryLayout)/page.js
+++ src/app/(secondaryLayout)/page.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const page = () => {
+const Page = () => {
     return (
         <div>
             Scecondary
@@ -8,4 +8,4 @@ const page = () => {
     );
 };
 
-export default page;
+export default Page;
