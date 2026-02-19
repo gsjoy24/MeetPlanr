c2cbb82f953efab1d55081fcac6326ac31dea418
Gour Chandra Saha <gour.joy24@gmail.com>
2026-02-19 15:12:49 +0600

    Refactor layout.js to include tracking scripts
    Updated metadata description and added Google Tag Manager script for tracking.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,24 +1,43 @@
 import './globals.css';
-export const metadata = {
-	title: 'MeetPlanr',
-	description:
-		'MeetPlanr: The open-source solution for effortless meeting scheduling and planning. Sync calendars, leverage smart algorithms, and boost collaboration. Join us now! 🚀 #MeetPlanr #SchedulingMadeEasy'
-};
+import Script from 'next/script';
 import { poppins } from '@/utils/fonts';
 import { AuthProvider } from '@/providers/AuthProvider';
 import { Toaster } from 'react-hot-toast';
 
+export const metadata = {
+  title: 'MeetPlanr',
+  description:
+    'MeetPlanr: The open-source solution for effortless meeting scheduling and planning. Sync calendars, leverage smart algorithms, and boost collaboration.'
+};
+
 export default function RootLayout({ children }) {
-	return (
-		<html lang="en">
-			<body className={poppins.className}>
-				<AuthProvider>
-					<div>
-						<main className="min-h-[calc(100vh-200px)]">{children}</main>
-					</div>
-					<Toaster position="top-center" reverseOrder={false} />
-				</AuthProvider>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body className={poppins.className}>
+        
+        {/* Google Tag Script */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17911165587"
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-ads-tag" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-17911165587');
+          `}
+        </Script>
+
+        <AuthProvider>
+          <main className="min-h-[calc(100vh-200px)]">
+            {children}
+          </main>
+          <Toaster position="top-center" reverseOrder={false} />
+        </AuthProvider>
+
+      </body>
+    </html>
+  );
 }
